5ed0d4eb515744c89d4d1ccc3e100ed401a08054
Hiago <hiagofss98@gmail.com>
2025-03-30 21:37:45 -0300

    Adjust quality gate

--- sonar-salesforce-pmd/src/main/java/org/sonar/salesforce/rule/GenericSalesforceRuleDefinition.java
+++ sonar-salesforce-pmd/src/main/java/org/sonar/salesforce/rule/GenericSalesforceRuleDefinition.java
@@ -19,135 +19,140 @@
  */
 
 package org.sonar.salesforce.rule;
-import org.sonar.salesforce.SalesforcePlugin;
 
-// import org.sonar.api.config.Settings;
-import org.sonar.api.server.rule.RulesDefinition;
-// import org.sonar.api.rule.RuleKey;
-// import org.sonar.api.server.rule.RuleScope;
-import org.sonar.api.rule.RuleStatus;
-// import org.sonar.api.server.rule.RuleParamType;
-import org.sonar.api.rule.Severity;
-import org.sonar.api.rules.RuleType;
-// import org.sonar.api.server.ServerSide;
-import org.sonar.api.utils.log.Logger;
-import org.sonar.api.utils.log.Loggers;
 import java.io.InputStream;
+
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
+
+import org.apache.commons.lang3.StringUtils;
 import org.codehaus.staxmate.SMInputFactory;
 import org.codehaus.staxmate.in.SMHierarchicCursor;
 import org.codehaus.staxmate.in.SMInputCursor;
-import org.codehaus.staxmate.SMInputFactory;
-import org.apache.commons.lang3.StringUtils;
-import org.sonar.api.batch.rule.internal.NewRule;
-
-
+// import org.sonar.api.server.rule.RuleParamType;
+import org.sonar.api.rule.Severity;
+import org.sonar.api.rules.RuleType;
+// import org.sonar.api.config.Settings;
+import org.sonar.api.server.rule.RulesDefinition;
+// import org.sonar.api.server.ServerSide;
+import org.sonar.api.utils.log.Logger;
+import org.sonar.api.utils.log.Loggers;
+import org.sonar.salesforce.SalesforcePlugin;
 
 public class GenericSalesforceRuleDefinition implements RulesDefinition {
 
-    private static final Logger LOGGER = Loggers.get(GenericSalesforceRuleDefinition.class);
-    // private final Settings settings;
+  private static final Logger LOGGER = Loggers.get(GenericSalesforceRuleDefinition.class);
 
-    @Override
-    public void define(Context context) {
+  // private final Settings settings;
 
-        // this.settings = context.settings();
+  @Override
+  public void define(Context context) {
 
-        LOGGER.debug("PMD: In GenericSalesforceRuleDefinition define");
-        NewRepository repository = context.createRepository(SalesforcePlugin.REPOSITORY_KEY, SalesforcePlugin.LANGUAGE_KEY).setName("Salesforce Analyzer");
-        LOGGER.debug("PMD: Created repo");
+    // this.settings = context.settings();
 
-        // repository.createRule("GenericSalesforcePmdViolation")
-        // .setName("Generic Salesforce Pmd Violation")
-        // .setHtmlDescription("Generic rule")
-        // .setSeverity(Severity.MINOR);
-        // .setTags("style", )
+    LOGGER.debug("PMD: In GenericSalesforceRuleDefinition define");
+    NewRepository repository =
+        context
+            .createRepository(SalesforcePlugin.REPOSITORY_KEY, SalesforcePlugin.LANGUAGE_KEY)
+            .setName("Salesforce Analyzer");
+    LOGGER.debug("PMD: Created repo");
 
+    // repository.createRule("GenericSalesforcePmdViolation")
+    // .setName("Generic Salesforce Pmd Violation")
+    // .setHtmlDescription("Generic rule")
+    // .setSeverity(Severity.MINOR);
+    // .setTags("style", )
 
-        addRulesFromPmdRuleset(repository, "/org/sonar/salesforce/rulesets/apex/bestpractices.xml", RuleType.BUG);
-        addRulesFromPmdRuleset(repository, "/org/sonar/salesforce/rulesets/apex/codestyle.xml", RuleType.CODE_SMELL);
-        addRulesFromPmdRuleset(repository, "/org/sonar/salesforce/rulesets/apex/design.xml", RuleType.CODE_SMELL);
-        addRulesFromPmdRuleset(repository, "/org/sonar/salesforce/rulesets/apex/errorprone.xml", RuleType.BUG);
-        addRulesFromPmdRuleset(repository, "/org/sonar/salesforce/rulesets/apex/performance.xml", RuleType.BUG);
-        addRulesFromPmdRuleset(repository, "/org/sonar/salesforce/rulesets/apex/security.xml", RuleType.VULNERABILITY);
-        // addRulesFromPmdRuleset(repository, "/org/sonar/salesforce/rulesets/apex/documentation.xml", RuleType.CODE_SMELL);
+    addRulesFromPmdRuleset(
+        repository, "/org/sonar/salesforce/rulesets/apex/bestpractices.xml", RuleType.BUG);
+    addRulesFromPmdRuleset(
+        repository, "/org/sonar/salesforce/rulesets/apex/codestyle.xml", RuleType.CODE_SMELL);
+    addRulesFromPmdRuleset(
+        repository, "/org/sonar/salesforce/rulesets/apex/design.xml", RuleType.CODE_SMELL);
+    addRulesFromPmdRuleset(
+        repository, "/org/sonar/salesforce/rulesets/apex/errorprone.xml", RuleType.BUG);
+    addRulesFromPmdRuleset(
+        repository, "/org/sonar/salesforce/rulesets/apex/performance.xml", RuleType.BUG);
+    addRulesFromPmdRuleset(
+        repository, "/org/sonar/salesforce/rulesets/apex/security.xml", RuleType.VULNERABILITY);
+    addRulesFromPmdRuleset(
+        repository, "/org/sonar/salesforce/rulesets/apex/documentation.xml", RuleType.CODE_SMELL);
 
+    repository.done();
+  }
 
-       repository.done();
-
-   }
-
-    public static String priorityToSeverity(String priority, Integer critical, Integer major) {
-        int score = Integer.parseInt(priority);
-        if (critical > 0 && score <= critical) {
-            return Severity.CRITICAL;
-        } else if (major > 0 && score <= major) {
-            return Severity.MAJOR;
-        } else {
-            return Severity.MINOR;
-        }
+  public static String priorityToSeverity(String priority, Integer critical, Integer major) {
+    int score = Integer.parseInt(priority);
+    if (critical > 0 && score <= critical) {
+      return Severity.CRITICAL;
+    } else if (major > 0 && score <= major) {
+      return Severity.MAJOR;
+    } else {
+      return Severity.MINOR;
     }
+  }
 
-    public void addRulesFromPmdRuleset(NewRepository repository, String resourcePath, RuleType ruleType){
-        InputStream inputStream = getClass().getResourceAsStream(resourcePath);
+  public void addRulesFromPmdRuleset(
+      NewRepository repository, String resourcePath, RuleType ruleType) {
+    InputStream inputStream = getClass().getResourceAsStream(resourcePath);
 
-        XMLInputFactory xmlFactory = XMLInputFactory.newInstance();
-        xmlFactory.setProperty(XMLInputFactory.IS_COALESCING, Boolean.TRUE);
-        xmlFactory.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE, Boolean.FALSE);
-        xmlFactory.setProperty(XMLInputFactory.SUPPORT_DTD, Boolean.FALSE);
-        xmlFactory.setProperty(XMLInputFactory.IS_VALIDATING, Boolean.FALSE);
-        SMInputFactory inputFactory = new SMInputFactory(xmlFactory);
+    XMLInputFactory xmlFactory = XMLInputFactory.newInstance();
+    xmlFactory.setProperty(XMLInputFactory.IS_COALESCING, Boolean.TRUE);
+    xmlFactory.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE, Boolean.FALSE);
+    xmlFactory.setProperty(XMLInputFactory.SUPPORT_DTD, Boolean.FALSE);
+    xmlFactory.setProperty(XMLInputFactory.IS_VALIDATING, Boolean.FALSE);
+    SMInputFactory inputFactory = new SMInputFactory(xmlFactory);
 
-        LOGGER.debug("Reading rule file");
+    LOGGER.debug("Reading rule file");
 
-        try {
-            SMHierarchicCursor rootC = inputFactory.rootElementCursor(inputStream);
-            rootC.advance(); // ruleset
+    try {
+      SMHierarchicCursor rootC = inputFactory.rootElementCursor(inputStream);
+      rootC.advance(); // ruleset
 
-            SMInputCursor ruleCursor = rootC.childCursor();
+      SMInputCursor ruleCursor = rootC.childCursor();
 
-            while (ruleCursor.getNext() != null) {
-                String nodeName = ruleCursor.getLocalName();
-                if ("rule".equals(nodeName)) {
+      while (ruleCursor.getNext() != null) {
+        String nodeName = ruleCursor.getLocalName();
+        if ("rule".equals(nodeName)) {
 
-                    String name = StringUtils.trim(ruleCursor.getAttrValue("name"));
-                    String message = StringUtils.trim(ruleCursor.getAttrValue("message"));
-                    String url = StringUtils.trim(ruleCursor.getAttrValue("externalInfoUrl"));
-                    String description = "";
-                    String priority = "";
+          String name = StringUtils.trim(ruleCursor.getAttrValue("name"));
+          String message = StringUtils.trim(ruleCursor.getAttrValue("message"));
+          String url = StringUtils.trim(ruleCursor.getAttrValue("externalInfoUrl"));
+          String description = "";
+          String priority = "";
 
-                    SMInputCursor ruleData = ruleCursor.childCursor();
+          SMInputCursor ruleData = ruleCursor.childCursor();
 
-                    while (ruleData.getNext() != null) {
-                        String childName = ruleData.getLocalName();
-                        if ("description".equals(childName)) {
-                            description = StringUtils.trim(ruleData.collectDescendantText(false));
-                        } else if ("priority".equals(childName)) {
-                            priority = StringUtils.trim(ruleData.collectDescendantText(false));
-                        }
-                        // example
-                        // tags ?
-                    }
-                    // String settingsKey = "sonar.salesforce.rule." + name + ".priority";
-                    // LOGGER.debug("Looking for setting {}", settingsKey);
-                    // if (settings.hasKey(settingsKey)){
-                    //     LOGGER.debug("Found priority override for {}", name);
-                    //     priority = settings.getString(settingsKey);
-                    // }
-
-
-                    NewRule rule = repository.createRule(name)
-                        .setName(message)
-                        .setType(ruleType)
-                        .setSeverity(priorityToSeverity(priority, 2, 4))
-                        .setActivatedByDefault(true)
-                        .setMarkdownDescription(description);
-                }
+          while (ruleData.getNext() != null) {
+            String childName = ruleData.getLocalName();
+            if ("description".equals(childName)) {
+              description = StringUtils.trim(ruleData.collectDescendantText(false));
+            } else if ("priority".equals(childName)) {
+              priority = StringUtils.trim(ruleData.collectDescendantText(false));
             }
-        } catch (XMLStreamException e) {
-            LOGGER.debug("PMD: Failed to load apex rules file");
-            throw new IllegalStateException("XML is not valid", e);
+            // example
+            // tags ?
+          }
+          // String settingsKey = "sonar.salesforce.rule." + name + ".priority";
+          // LOGGER.debug("Looking for setting {}", settingsKey);
+          // if (settings.hasKey(settingsKey)){
+          // LOGGER.debug("Found priority override for {}", name);
+          // priority = settings.getString(settingsKey);
+          // }
+
+          NewRule rule =
+              repository
+                  .createRule(name)
+                  .setName(message)
+                  .setType(ruleType)
+                  .setSeverity(priorityToSeverity(priority, 2, 4))
+                  .setActivatedByDefault(true)
+                  .setMarkdownDescription(description);
         }
+      }
+    } catch (XMLStreamException e) {
+      LOGGER.debug("PMD: Failed to load apex rules file");
+      throw new IllegalStateException("XML is not valid", e);
     }
+  }
 }
--- sonar-salesforce-pmd/src/main/java/org/sonar/salesforce/rule/SalesforceProfile.java
+++ sonar-salesforce-pmd/src/main/java/org/sonar/salesforce/rule/SalesforceProfile.java
@@ -22,62 +22,78 @@ package org.sonar.salesforce.rule;
 
 import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition;
 import org.sonar.salesforce.SalesforcePlugin;
-import org.sonar.api.server.rule.RulesDefinition;
-import java.util.Iterator;
-import java.util.List;
-
 
 public class SalesforceProfile implements BuiltInQualityProfilesDefinition {
 
-	public void define(Context context) {
-		NewBuiltInQualityProfile profile = context.createBuiltInQualityProfile("Salesforce", SalesforcePlugin.LANGUAGE_KEY);
-		profile.setDefault(true);
+  public void define(Context context) {
+    NewBuiltInQualityProfile profile =
+        context.createBuiltInQualityProfile("Salesforce", SalesforcePlugin.LANGUAGE_KEY);
+    profile.setDefault(true);
 
-
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "IfElseStmtsMustUseBraces");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "WhileLoopsMustUseBraces");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexXSSFromURLParam");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexSuggestUsingNamedCred");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidSoslInLoops");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexSOQLInjection");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexXSSFromEscapeFalse");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "EmptyIfStmt");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ExcessiveClassLength");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexBadCrypto");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexUnitTestShouldNotUseSeeAllDataTrue");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "NcssMethodCount");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "StdCyclomaticComplexity");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexCSRF");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidLogicInTrigger");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ExcessiveParameterList");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "EmptyCatchBlock");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "NcssConstructorCount");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "EmptyWhileStmt");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexUnitTestClassShouldHaveAsserts");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ClassNamingConventions");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ForLoopsMustUseBraces");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "CyclomaticComplexity");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexInsecureEndpoint");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "EmptyStatementBlock");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidGlobalModifier");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "EmptyTryOrFinallyBlock");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexCRUDViolation");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexOpenRedirect");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexSharingViolations");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidDirectAccessTriggerMap");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidDmlStatementsInLoops");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "IfStmtsMustUseBraces");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "VariableNamingConventions");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "MethodWithSameNameAsEnclosingClass");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ExcessivePublicCount");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "NcssTypeCount");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "MethodNamingConventions");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidSoqlInLoops");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidHardcodingId");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidDeeplyNestedIfStmts");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "TooManyFields");
-		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexDangerousMethods");
-		profile.done();
-	}
-
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "IfElseStmtsMustUseBraces");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "WhileLoopsMustUseBraces");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexXSSFromURLParam");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexSuggestUsingNamedCred");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexSOQLInjection");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "EmptyIfStmt");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ExcessiveClassLength");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexBadCrypto");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexUnitTestShouldNotUseSeeAllDataTrue");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "NcssMethodCount");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "StdCyclomaticComplexity");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexCSRF");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidLogicInTrigger");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ExcessiveParameterList");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "EmptyCatchBlock");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "NcssConstructorCount");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "EmptyWhileStmt");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexUnitTestClassShouldHaveAsserts");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ClassNamingConventions");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ForLoopsMustUseBraces");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "CyclomaticComplexity");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexInsecureEndpoint");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "EmptyStatementBlock");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidGlobalModifier");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "EmptyTryOrFinallyBlock");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexCRUDViolation");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexOpenRedirect");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexSharingViolations");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidDirectAccessTriggerMap");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidDebugStatements");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidNonRestrictiveQueries");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "EagerlyLoadedDescribeSObjectResult");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "OperationWithHighCostInLoop");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "OperationWithLimitsInLoop");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidNonExistentAnnotations");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidStatefulDatabaseResult");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "InaccessibleAuraEnabledGetter");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "OverrideBothEqualsAndHashcode");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "TestMethodsMustBeInTestClasses");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "IfStmtsMustUseBraces");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "MethodWithSameNameAsEnclosingClass");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ExcessivePublicCount");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "NcssTypeCount");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "MethodNamingConventions");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidHardcodingId");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidDeeplyNestedIfStmts");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "TooManyFields");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexDangerousMethods");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "UnusedMethod");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "CognitiveComplexity");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "NcssConstructorCount");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexAssertionsShouldIncludeMessage");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexUnitTestClassShouldHaveRunAs");
+    profile.activateRule(
+        SalesforcePlugin.REPOSITORY_KEY, "ApexUnitTestMethodShouldHaveIsTestAnnotation");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "DebugsShouldUseLoggingLevel");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "UnusedLocalVariable");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "QueueableWithoutFinalizer");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "FieldDeclarationsShouldBeAtStart");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "FieldNamingConventions");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "FormalParameterNamingConventions");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "LocalVariableNamingConventions");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "OneDeclarationPerLine");
+    profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "PropertyNamingConventions");
+    profile.done();
+  }
 }
